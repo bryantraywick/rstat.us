3d4588caaa2aa7f9f396956ebe1ca8637c17ad04
Bryan Traywick <bryantraywick@gmail.com>
2013-04-25 15:59:03 -0400

    Added SECRET_TOKEN environment variable to 'config/deploy.rb'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,2 @@
 server 'traywick.railsmachine.managedmachine.com', :app, :web, :db, :primary => true
+default_environment['SECRET_TOKEN'] = "44c2069a5fe7b2e89b323b39c0924e11caa30c8df16965d1c5f91cbacc3afb0807e05c18861e0711643b65e9a209bb0ad218ad8c00abe4bad62faf1cee18bd02"
