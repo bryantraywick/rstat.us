1e2816e87e3bf33cda93ad1d0d142a0b5a390b64
Bryan Traywick <bryantraywick@gmail.com>
2013-04-26 11:53:26 -0400

    Added dj and god support.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,3 @@
-server 'traywick.railsmachine.managedmachine.com', :app, :web, :db, :primary => true
+server 'traywick.railsmachine.managedmachine.com', :app, :web, :dj, :db, :primary => true
 set :local_config, %w(config/config.yml)
+before 'god:restart', 'god:dj:stop'
